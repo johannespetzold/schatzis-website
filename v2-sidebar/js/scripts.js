f4e374ddf3d2146f2c26723a0f80b0d96bcf8775
Johannes Petzold <jpetzold@newrelic.com>
2015-04-21 21:58:53 -0700

    proportional dark side bars

--- v2-sidebar/js/scripts.js
+++ v2-sidebar/js/scripts.js
@@ -1,7 +1,8 @@
 var placeSideBar = function() {
   var docWidth = $(document).width();
   var maxWidth = parseInt($(".body").css("max-width"), 10);
-  var right = Math.max(0, (docWidth - maxWidth) / 2);
+  var padding = parseInt($(".body").css("padding-right"), 10);
+  var right = Math.max(padding, (docWidth - maxWidth) / 2);
   $("nav").css("right", right + "px");
 };
 
